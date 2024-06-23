cdcabd1bcc5e6c1c5fe64cca08a9aa9b9232d61f
vertunoff <anton.s.vertunov@gmail.com>
2024-06-23 04:47:55 +0300

    logging configured

--- database.py
+++ database.py
@@ -2,6 +2,14 @@ from sqlalchemy.orm import DeclarativeBase, sessionmaker
 from sqlalchemy import create_engine
 from dotenv import load_dotenv
 import os
+import logging
+
+logging.basicConfig(
+    filename='sqlalchemy.log',  # имя файла, куда будет записан лог
+    level=logging.INFO,  # уровень логирования
+    format='%(asctime)s - %(name)s - %(levelname)s - %(message)s'  # форматирование сообщений
+)
+logging.getLogger('sqlalchemy.engine').setLevel(logging.INFO)
 load_dotenv()
 
 DATABASE_URL = os.getenv('DATABASE_URL')
@@ -13,4 +21,4 @@ SessionLocal = sessionmaker(bind=engine)
 class Base(DeclarativeBase):
     def _repr(self, *fields):
         attrs = ', '.join(f'{field}={repr(getattr(self, field))}' for field in fields)
-        return f'<{self.__class__.__name__}({attrs})>'
+        return f'<{self.__class__.__name__}({attrs})>' 
